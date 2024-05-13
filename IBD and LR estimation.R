a7bed3ba7998ecffe28888d5b5e5b399e4cfb1e8
GiuliaCosenza <133686514+GiuliaCosenza@users.noreply.github.com>
2024-05-13 11:49:25 +0200

    Update IBD and LR estimation.R

--- IBD and LR estimation.R
+++ IBD and LR estimation.R
@@ -182,7 +182,10 @@ for (i in 1:length(pedlist)){
 #|  In this context, it is used to associate the values of k0, k1, and k2 obtained from checkpairwise          |#
 #|  with the corresponding relationship label.                                                                 |#
 #---------------------------------------------------------------------------------------------------------------#
-kinshipIBD <- data.frame("kinship" = c("UN", "PC", "FS", "HS,ZI,GP", "CO,PP,GG", "1C1R,PPP","C2"), "k0" = c(1,0,0.25,0.5,0.75,0.875,0.9375), "k1" = c(0,1,0.5,0.5,0.25,0.125,0.0625), "k2" = c(0,0,0.25,0,0,0,0))
+kinshipIBD <- data.frame("kinship" = c("UN", "PC", "FS", "HS,ZI,GP", "CO,PP,GG", "1C1R,PPP","C2"), 
+                         "k0" = c(1,0,0.25,0.5,0.75,0.875,0.9375), 
+                         "k1" = c(0,1,0.5,0.5,0.25,0.125,0.0625), 
+                         "k2" = c(0,0,0.25,0,0,0,0))
 
 for (i in 1:nrow(checkpairwise_result)) {
   RMSE = 10
